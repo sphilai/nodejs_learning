33510496a37f8356a9ae9b1f8bada01f3497bf46
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2018-05-23 19:56:59 +0700

    Use express instead http

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import http from 'http';
+import express from 'express';
+const app = express()
 
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World22\n');
-}).listen(1337, '127.0.0.1');
+app.get('/', (req, res) => res.send('Hello World!'))
 
-console.log('Server running at http://127.0.0.1:1337/');
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
